568b1bcdddb510ae51f75987961330b1cf520e87
gweizman <gweizman@gmail.com>
2014-09-18 17:26:25 +0300

    Trying to solve unicode issue

--- src/ClientRequestThread.java
+++ src/ClientRequestThread.java
@@ -56,7 +56,31 @@ public class ClientRequestThread extends Thread {
     
     // final String ERROR_FORMAT = "{'ERROR':'%s'}";
     // final String SUCCESS_MSG = "{'ACK':'Request performed'}\n";
-
+    
+    public static final String LAST_3_BYTE_UTF_CHAR = "\uFFFF";
+	public static final String REPLACEMENT_CHAR = "\uFFFD"; 
+	
+	public static String toValid3ByteUTF8String(String s)  {
+	    final int length = s.length();
+	    StringBuilder b = new StringBuilder(length);
+	    for (int offset = 0; offset < length; ) {
+	       final int codepoint = s.codePointAt(offset);
+	
+	       // do something with the codepoint
+	       if (codepoint > CharUtils.LAST_3_BYTE_UTF_CHAR.codePointAt(0)) {
+	           b.append(CharUtils.REPLACEMENT_CHAR);
+	       } else {
+	           if (Character.isValidCodePoint(codepoint)) {
+	               b.appendCodePoint(codepoint);
+	           } else {
+	               b.append(CharUtils.REPLACEMENT_CHAR);
+	           }
+	       }
+	       offset += Character.charCount(codepoint);
+	    }
+	    return b.toString();
+	}
+    
     /**
      * 
      * @param con the db connection
@@ -257,7 +281,7 @@ public class ClientRequestThread extends Thread {
             // send resultSet                    
                 //this.writer.write(createResponse(resultSet));
                 String response = createResponse(resultSet, 1, "");
-               //  System.out.println(response);
+                System.out.println("Resonse: " + response);
                 this.out.print(response);
             
             	logMSG(response, Level.FINEST);
