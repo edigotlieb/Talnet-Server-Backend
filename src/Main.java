8131b4bc89362e7e02b2c645694f764c9b69bccd
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-10-15 17:07:11 +0200

    backend v2 log lock file fix

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
 		FileHandler fh = new FileHandler("log" + new java.text.SimpleDateFormat("yyyyMMddHHmmss").format(new Date().getTime()) + ".txt");
 		fh.setFormatter(new SimpleFormatter());
 		Logger.getGlobal().addHandler(fh);
-		ThreadHandler.setLogLevel(Logger.getGlobal(), Level.INFO);
+		ThreadHandler.setLogLevel(Logger.getGlobal(), Level.FINEST);
 
 		Logger.getGlobal().log(Level.INFO, "file.encoding={0}", System.getProperty("file.encoding"));
 		Logger.getGlobal().log(Level.INFO, "Default Charset={0}", Charset.defaultCharset());
