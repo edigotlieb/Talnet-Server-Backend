c7c9570b85631c72cffb2d8baad8dbb74f42a98a
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-11-30 11:32:26 +0200

    debug

--- src/SQL/PreparedStatements/StatementPreparer.java
+++ src/SQL/PreparedStatements/StatementPreparer.java
@@ -16,7 +16,7 @@ public abstract class StatementPreparer {
 	public abstract void prepareStatement(PreparedStatement ps) throws SQLException;
 
 	protected void prepareStatement(PreparedStatement ps, ArgumentSet arguments, RequestArgumentStructureAssignment requestArguments, Credentials creds) throws SQLException {
-		System.out.println(ps.getParameterMetaData().getParameterCount());
+		System.out.println("DEBUG" + ps.getParameterMetaData().getParameterCount());
 		Iterator<Integer> it = arguments.keySet().iterator();
 		while (it.hasNext()) {
 			Argument argument = arguments.get(it.next());
